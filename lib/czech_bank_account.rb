b6063e51513ec699d71f152681bcb004fb9aea6d
Premysl Donat <pdonat@seznam.cz>
2018-12-07 12:07:04 +0100

    Bundler new

---
+++ lib/czech_bank_account.rb
@@ -0,0 +1,6 @@
+require "czech_bank_account/version"
+
+module CzechBankAccount
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/czech_bank_account/version.rb
@@ -0,0 +1,3 @@
+module CzechBankAccount
+  VERSION = "0.1.0"
+end
